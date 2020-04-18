2e724148720f597aa134f3f4faa3882722cc9929
rj14ng <rocco.jiang5@gmail.com>
2020-04-19 00:43:55 +0800

    Add comments

--- for/repeat.go
+++ for/repeat.go
@@ -1,5 +1,6 @@
 package iteration
 
+// Repeat takes a character and repeat count, returning the character repeated by that many times.
 func Repeat(character string, repeatCount int) string {
 	var repeated string
 	for i := 0; i < repeatCount; i++ {
