c37478938a9e1c3af1ddadff704086d35e1fd229
rj14ng <rocco.jiang5@gmail.com>
2020-04-19 18:35:19 +0800

    First TDD iteration

---
+++ arrays/sum.go
@@ -0,0 +1,10 @@
+package arrays
+
+// Sum returns the sum of the elements within an array.
+func Sum(numbers [5]int) int {
+	sum := 0
+	for i := 0; i < 5; i++ {
+		sum += numbers[i]
+	}
+	return sum
+}
