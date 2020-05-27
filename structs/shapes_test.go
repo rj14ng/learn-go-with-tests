43a4abc0763da02ad9add50e9218ae85d0ce54d1
Rocco Jiang <rocco.jiang5@gmail.com>
2020-05-28 00:11:58 +0800

    Add Triangle tests and refactor for clearer output

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -5,35 +5,45 @@ import "testing"
 func TestPerimeter(t *testing.T) {
 	// Slice of anonymous structs, perimeterTests
 	perimeterTests := []struct {
-		shape Shape
-		want  float64
+		name         string
+		shape        Shape
+		hasPerimeter float64
 	}{
-		{Rectangle{10, 10}, 40.0},
-		{Circle{5}, 31.415926535897932},
+		{name: "Rectangle", shape: Rectangle{10, 10}, hasPerimeter: 40.0},
+		{name: "Circle", shape: Circle{5}, hasPerimeter: 31.415926535897932},
+		{name: "Triangle", shape: Triangle{5, 12, 13}, hasPerimeter: 30.0},
 	}
 
 	for _, tt := range perimeterTests {
-		got := tt.shape.Perimeter()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
+		// Using tt.name from the case to use it as the 't.Run' test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Perimeter()
+			if got != tt.hasPerimeter {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasPerimeter)
+			}
+		})
 	}
 }
 
 func TestArea(t *testing.T) {
 	// Slice of anonymous structs, areaTests
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 314.1592653589793},
+		{name: "Rectangle", shape: Rectangle{12, 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{3, 4, 5}, hasArea: 6.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
+		// Using tt.name from the case to use it as the 't.Run' test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
 	}
 }
