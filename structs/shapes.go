82548b90f2e5662c9e673f80e4b77f9d0561765c
Rocco Jiang <rocco.jiang5@gmail.com>
2020-05-28 00:11:27 +0800

    Add Triangle struct

--- structs/shapes.go
+++ structs/shapes.go
@@ -8,7 +8,7 @@ type Shape interface {
 	Area() float64
 }
 
-// A Rectangle represents a rectangle shape with a width and height.
+// A Rectangle represents a rectangle shape, defined with a width and height.
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -24,7 +24,7 @@ func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
-// A Circle represents a circle shape with a radius.
+// A Circle represents a circle shape, defined with a radius.
 type Circle struct {
 	Radius float64
 }
@@ -37,4 +37,20 @@ func (c Circle) Perimeter() float64 {
 // Area returns the area of a Circle.
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+// A Triangle represents a triangle shape, defined with its 3 sides.
+type Triangle struct {
+	SideA, SideB, SideC float64
+}
+
+// Perimeter returns the perimeter of a Triangle.
+func (t Triangle) Perimeter() float64 {
+	return t.SideA + t.SideB + t.SideC
+}
+
+// Area returns the area of a Triangle, using Heron's formula.
+func (t Triangle) Area() float64 {
+	p := t.Perimeter() / 2.0 // Semiperimeter
+	return math.Sqrt(p * (p - t.SideA) * (p - t.SideB) * (p - t.SideC))
 }
