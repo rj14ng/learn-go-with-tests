c54dbfa572556ea3da23f640892bd181889d35dc
Rocco Jiang <rocco.jiang5@gmail.com>
2020-05-27 23:54:10 +0800

    Refactor with table driven tests

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -3,41 +3,37 @@ package structs
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	checkPerimeter := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Perimeter()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	// Slice of anonymous structs, perimeterTests
+	perimeterTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{10, 10}, 40.0},
+		{Circle{5}, 31.415926535897932},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{10, 10}
-		checkPerimeter(t, rectangle, 40.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{5}
-		checkPerimeter(t, circle, 31.415926535897932)
-	})
+	for _, tt := range perimeterTests {
+		got := tt.shape.Perimeter()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	// Slice of anonymous structs, areaTests
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
 }
