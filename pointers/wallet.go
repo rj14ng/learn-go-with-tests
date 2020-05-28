dff9f64b1d53f2dfa224929739bdc790e1f8742a
Rocco Jiang <rocco.jiang5@gmail.com>
2020-05-28 18:42:41 +0800

    Add Bitcoin int type

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,13 +1,26 @@
 package pointers
 
-type Wallet struct {
-	balance int
+import "fmt"
+
+// A Bitcoin represents the number of Bitcoins.
+type Bitcoin int
+
+// Implement Stringer on Bitcoin with String method
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
 
-func (w *Wallet) Deposit(amount int) {
+// A Wallet stores the number of Bitcoins someone owns.
+type Wallet struct {
+	balance Bitcoin
+}
+
+// Deposit adds a number of Bitcoin to a Wallet.
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+// Balance returns the number of Bitcoin within a Wallet.
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
